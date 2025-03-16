dad24df3a248a5c4c5663a49ce06871bea66bbaf
IbnuHajar_hl <152904947+Hanalunnaya@users.noreply.github.com>
2025-03-16 19:33:09 +0700

    Add files via upload

---
+++ include/sidebar.php
@@ -0,0 +1,65 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+<link href="css/custom.css" rel="stylesheet">
+
+<!-- Main SB Admin 2 CSS -->
+<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+<link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+<!-- Custom CSS (Add this after the main CSS file) -->
+<link href="css/custom.css" rel="stylesheet">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <img src="" alt="Logo" width="45">
+        <div class="sidebar-brand-text mx-3">Koperasi Pegawai</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <li class="nav-item active">
+        <a class="nav-link" href="costumer.php">
+            <i class="fa fa-users" aria-hidden="true"></i>
+            <span>Costumer</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+ 
+    <li class="nav-item active">
+        <a class="nav-link" href="item.php">
+            <i class="fa fa-square" aria-hidden="true"></i>
+            <span>Item</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+    
+    <li class="nav-item active">
+        <a class="nav-link" href="user.php">
+            <i class="fas fa-fw fa-user"></i>
+            <span>User</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+</ul>
